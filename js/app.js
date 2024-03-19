7eaae1f9bb259d9e7c05a285c39bac2fb1d1c134
Francesca-Barletta <wallebar@gmail.com>
2024-03-19 17:11:45 +0100

    add snack 2

--- js/app.js
+++ js/app.js
@@ -22,18 +22,18 @@ esempio:
 }*/
 //snack1
 
-function vipCard(table, nome, place){
+function vipCard(table, nome, place) {
     const vip = {
         nomeTavolo: table,
         nomeOspite: nome,
-        posto:place
+        posto: place
     }
     return vip
 }
-let tavoloVip = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 
-'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+let tavoloVip = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez',
+    'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
 let vipList = []
-for(let i = 0; i < tavoloVip.length; i++){
+for (let i = 0; i < tavoloVip.length; i++) {
     const table = 'Tavolo VIP';
     const name = tavoloVip[i];
     const place = i
@@ -42,9 +42,6 @@ for(let i = 0; i < tavoloVip.length; i++){
     vipList.push(persona)
     console.log(persona)
 }
-
-
-
 console.log(vipList);
 
 
@@ -75,4 +72,51 @@ Id   Name                    Grades
 102  Piero della Francesca   50
 120  Francesca da Polenta    84*/
 
+function createSign(Number, person, vote) {
+    const student = {
+        id: Number,
+        name: person,
+        grades: vote
+    }
+    return student
+}
+let studentList = []
 
+const marco = createSign(213, 'Marco della Rovere', 78);
+const paola = createSign(110, 'Paola Cortellessa', 96);
+const andrea = createSign(250, 'Andrea Mantegna', 48);
+const gaia = createSign(145, 'Gaia Borromini', 74);
+const luigi = createSign(196, 'Luigi Grimaldello', 68);
+const piero = createSign(102, 'Piero Della Francesca', 50);
+const francesca = createSign(120, 'Francesca Da Polenta', 84);
+studentList.push(marco, paola, andrea, gaia, luigi, piero, francesca);
+console.log(studentList);
+//console.log(marco.name);
+
+
+function capitalize(string) {
+    let newName = string.toUpperCase();
+    return newName;
+}
+
+studentList.forEach((element, i) => {
+    const upperName = capitalize(element.name);
+    studentList[i].name = upperName;
+})
+//console.log(studentList);
+
+let overSeventy = [];
+for (let i = 0; i < studentList.length; i++) {
+    const singleStudent = studentList[i]
+    //console.log(singleStudent);
+    if (singleStudent.grades > 70) {
+        overSeventy.push(singleStudent);
+    }
+} console.log(overSeventy);
+const seventyAndId = overSeventy.filter((element, i, array) =>{
+    const singleStudent = overSeventy[i];
+    if(singleStudent.id > 120){
+        return true
+    }
+});
+console.log(seventyAndId);
